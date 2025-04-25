569666030462902b4ff37e53ed4a1321bda89699
Ethutami <ethikautamis@gmail.com>
2025-04-25 16:12:33 +0800

    Create component.tsx

---
+++ slider/component.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Image from "next/image";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+const imageslides = [
+    { image: '/slide1.png' },
+    { image: '/slide2.png' },
+    { image: '/slide3.png' }
+]
+
+export const ImageSlider = () => {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+    const prevSlide = (): void => {
+        setCurrentIndex(
+            (prevIndex) => (prevIndex - 1 + imageslides.length) % imageslides.length
+        );
+    };
+
+    const nextSlide = (): void => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % imageslides.length);
+    };
+
+    // Autoplay effect
+    useEffect(() => {
+        const interval = setInterval(() => {
+            nextSlide();
+        }, 5000);
+
+        return () => clearInterval(interval); // clear interval saat unmount
+    }, []);
+
+    return (
+        <div className="flex flex-row  w-full mx-auto m-30 items-center justify-around">
+            <button onClick={prevSlide}>
+                <ChevronLeft className="text-gray-400 " size={32} />
+            </button>
+
+            <div>
+                <div className="flex items-center justify-center relative ">
+                    <Image
+                        src={imageslides[currentIndex].image}
+                        alt={`Slider Image ${currentIndex + 1}`}
+                        objectFit="cover"
+                        className="rounded-l-md"
+                        width={800}
+                        height={800}
+                    />
+                </div>
+                <div className="flex justify-center mt-4 space-x-2">
+                    {imageslides.map((_, i) => (
+                        <span
+                            key={i}
+                            className="dot-indicator"
+                            data-active={i === currentIndex}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <button onClick={nextSlide}>
+                <ChevronRight className="text-gray-400 " size={32} />
+            </button>
+        </div>
+    )
+}
